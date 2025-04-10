0d62280b08078eab76d48d35e0ce56d3cb4c2277
shunya <shabanchick@gmail.com>
2025-04-10 12:31:15 +0300

    init project

---
+++ eslint.config.js
@@ -0,0 +1,114 @@
+import eslint from '@eslint/js';
+import globals from 'globals';
+import reactHooks from 'eslint-plugin-react-hooks';
+import reactRefresh from 'eslint-plugin-react-refresh';
+import tseslint from 'typescript-eslint';
+import typescriptEslintEslintPlugin from '@typescript-eslint/eslint-plugin';
+import tsParser from '@typescript-eslint/parser';
+import eslintConfigPrettier from 'eslint-config-prettier';
+import importPlugin from 'eslint-plugin-import';
+import eslintPluginPrettierRecommended from 'eslint-plugin-prettier/recommended';
+import simpleImportSort from 'eslint-plugin-simple-import-sort';
+import unusedImports from 'eslint-plugin-unused-imports';
+import * as tsImportResolver from 'eslint-import-resolver-typescript';
+
+export default tseslint.config(
+  { ignores: ['dist'] },
+  ...tseslint.config(eslint.configs.recommended, ...tseslint.configs.recommended),
+  eslintConfigPrettier,
+  importPlugin.flatConfigs.recommended,
+  {
+    files: ['**/*.{ts,tsx}'],
+    plugins: {
+      '@typescript-eslint': typescriptEslintEslintPlugin,
+      'simple-import-sort': simpleImportSort,
+      'unused-imports': unusedImports,
+      'eslint-import-resolver-typescript': tsImportResolver,
+      'react-hooks': reactHooks,
+      'react-refresh': reactRefresh,
+    },
+    languageOptions: {
+      parser: tsParser,
+      ecmaVersion: 'latest',
+      sourceType: 'module',
+      globals: globals.browser,
+
+      parserOptions: {
+        project: 'tsconfig.json',
+      },
+    },
+    settings: {
+      'import/resolver': {
+        typescript: true,
+        node: true,
+      },
+    },
+    rules: {
+      ...reactHooks.configs.recommended.rules,
+      'react-refresh/only-export-components': ['warn', { allowConstantExport: true }],
+      'no-unused-vars': 'off',
+      '@typescript-eslint/await-thenable': 'error',
+      '@typescript-eslint/no-explicit-any': 'error',
+      '@typescript-eslint/no-unused-vars': 'off',
+      '@typescript-eslint/no-non-null-assertion': 'warn',
+      '@typescript-eslint/no-empty-object-type': 'off',
+
+      '@typescript-eslint/explicit-function-return-type': 'off',
+      '@typescript-eslint/explicit-module-boundary-types': 'off',
+
+      'import/first': 'warn',
+      'import/newline-after-import': 'warn',
+      'import/no-duplicates': 'warn',
+      'unused-imports/no-unused-imports': 'warn',
+      'simple-import-sort/imports': [
+        'warn',
+        {
+          groups: [['^\\u0000', '^@?\\w', '^[^.]', '^\\.']],
+        },
+      ],
+
+      camelcase: 'error',
+      'consistent-return': 'error',
+      'default-case': 'off',
+      eqeqeq: 'error',
+      'max-classes-per-file': 'off',
+      'no-console': 'off',
+      'no-eval': 'error',
+      'no-alert': 'error',
+      'no-compare-neg-zero': 'warn',
+      'no-case-declarations': 'off',
+      'no-else-return': 'warn',
+      'no-empty': 'warn',
+      'no-extend-native': 'error',
+      'no-invalid-this': 'error',
+      'no-iterator': 'error',
+      'no-labels': 'error',
+      'no-loop-func': 'error',
+      'no-new': 'error',
+      'no-new-func': 'error',
+      'no-new-wrappers': 'error',
+      'no-prototype-builtins': 'off',
+      'no-self-compare': 'error',
+      'no-sequences': 'error',
+      'no-throw-literal': 'error',
+      'no-unmodified-loop-condition': 'error',
+      'no-useless-concat': 'warn',
+      'no-useless-escape': 'warn',
+      'no-useless-return': 'warn',
+      'no-var': 'error',
+      'no-void': 'error',
+      'no-with': 'error',
+      'spaced-comment': 'warn',
+
+      curly: ['error', 'all'],
+    },
+  },
+  {
+    files: ['**/*.js', '**/*.mjs'],
+    rules: {
+      '@typescript-eslint/no-require-imports': 'off',
+      'no-undef': 'off',
+    },
+  },
+  eslintPluginPrettierRecommended,
+);
