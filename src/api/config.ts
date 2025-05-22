c3952d72d39647ac2cfdd9210c7b7de5c502fe04
shunya <shabanchick@gmail.com>
2025-05-22 17:03:53 +0300

    feat: changed axios config

--- src/api/config.ts
+++ src/api/config.ts
@@ -1,3 +1,8 @@
 import axios from 'axios';
 
-export const api = axios.create({});
+export const api = axios.create({ baseURL: 'https://cloud-api.yandex.net/v1/disk/resources' });
+
+api.interceptors.request.use((config) => {
+  config.headers.Authorization = `OAuth ${import.meta.env.VITE_O_AUTH}`;
+  return config;
+});
--- src/api/services.ts
+++ src/api/services.ts
@@ -1,17 +1,16 @@
+import axios from 'axios';
 import { api } from './config';
 import { GetDownloadLinkResponse, GetUploadLinkResponse } from './types';
 
 export const getUploadLink = (fileName: string, overwrite: boolean = false) => {
-  return api.get<GetUploadLinkResponse>(`https://cloud-api.yandex.net/v1/disk/resources/upload`, {
-    headers: { Authorization: `OAuth ${import.meta.env.VITE_O_AUTH}` },
+  return api.get<GetUploadLinkResponse>('/upload', {
     params: { path: fileName, overwrite },
   });
 };
 
-export const uploadFile = (uploadLink: string, data: FormData) => api.put(uploadLink, data);
+export const uploadFile = (uploadLink: string, data: FormData) => axios.put(uploadLink, data);
 
 export const getDownloadLink = (path: string) =>
-  api.get<GetDownloadLinkResponse>(`https://cloud-api.yandex.net/v1/disk/resources/download`, {
-    headers: { Authorization: `OAuth ${import.meta.env.VITE_O_AUTH}` },
+  api.get<GetDownloadLinkResponse>('/download', {
     params: { path },
   });
